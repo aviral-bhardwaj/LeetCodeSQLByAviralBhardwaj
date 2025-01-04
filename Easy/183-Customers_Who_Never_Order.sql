691d390dc0fa2e6abd60c1fb8881988ba125a022
Aviral Bhardwaj <50884868+aviral-bhardwaj@users.noreply.github.com>
2025-01-04 22:46:20 +0530

    Update 183-Customers_Who_Never_Order.sql

--- Easy/183-Customers_Who_Never_Order.sql
+++ Easy/183-Customers_Who_Never_Order.sql
@@ -26,11 +26,7 @@ Write a solution to find all customers who never order anything.
 Return the result table in any order.
 
 The result format is in the following example.
-
- 
-
-Example 1:
-
+###############  Example 1: ###############
 Input: 
 Customers table:
 +----+-------+
