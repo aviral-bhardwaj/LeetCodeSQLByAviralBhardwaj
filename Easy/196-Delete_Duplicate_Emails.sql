d03ae1d964eee201f1b9e12b1f16f6b3ecb99cca
Aviral Bhardwaj <50884868+aviral-bhardwaj@users.noreply.github.com>
2025-01-05 22:00:26 +0530

    Update 196-Delete_Duplicate_Emails.sql

--- Easy/196-Delete_Duplicate_Emails.sql
+++ Easy/196-Delete_Duplicate_Emails.sql
@@ -17,7 +17,6 @@ For Pandas users, please note that you are supposed to modify Person in place.
 After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
 
 The result format is in the following example.
-
  
 
 Example 1:
