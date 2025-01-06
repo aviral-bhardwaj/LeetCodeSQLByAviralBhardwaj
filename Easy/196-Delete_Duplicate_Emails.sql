7e198d80c6889d790b39755b69d48949be456264
Aviral Bhardwaj <50884868+aviral-bhardwaj@users.noreply.github.com>
2025-01-06 23:24:07 +0530

    Update 196-Delete_Duplicate_Emails.sql

--- Easy/196-Delete_Duplicate_Emails.sql
+++ Easy/196-Delete_Duplicate_Emails.sql
@@ -9,7 +9,8 @@ Table: Person
 +-------------+---------+
 id is the primary key (column with unique values) for this table.
 Each row of this table contains an email. The emails will not contain uppercase letters.
- 
+
+
 Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
 For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
 For Pandas users, please note that you are supposed to modify Person in place.
