3ae7cda64a6f9f719980ad70dfabe3a0eb43a0c9
Aviral Bhardwaj <50884868+aviral-bhardwaj@users.noreply.github.com>
2025-04-14 13:31:44 +0530

    Create Day1 Practice SampleSuperstoredata.sql

---
+++ SQL-Learning_AKB/Day 1/Day1 Practice SampleSuperstoredata.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Orders (
+    Row_ID INT,
+    Order_ID VARCHAR(20),
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(20),
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50),
+    Country_Region VARCHAR(50),
+    City VARCHAR(100),
+    State VARCHAR(50),
+    Postal_Code VARCHAR(20),
+    Region VARCHAR(20),
+    Product_ID VARCHAR(20),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255),
+    Sales DECIMAL(10, 4),
+    Quantity INT,
+    Discount DECIMAL(4, 2),
+    Profit DECIMAL(10, 4)
+);
+
+
